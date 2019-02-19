d393e092f72ab5d4d6f59792e487f69acff7a868
Payal Gupta <payal2.gupta@paytm.com>
2019-02-19 17:29:57 +0530

    url changes..

--- Example/GPVideoPlayer/ViewController.swift
+++ Example/GPVideoPlayer/ViewController.swift
@@ -18,12 +18,11 @@ class ViewController: UIViewController {
         if let player = GPVideoPlayer.initialize(with: self.mediaView.bounds) {
             self.mediaView.addSubview(player)
             
-            let url1 = URL(string: "https://san-sit-origin-mp5content.quickplay.com/singtel/ss/u/HOOQ/donottouch/sglavodT1/output_hls_r.ism/index.m3u8")!
-            let url2 = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!
+            let url1 = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!
             let videoFilePath = Bundle.main.path(forResource: "video", ofType: "mp4")
-            let url3 = URL(fileURLWithPath: videoFilePath!)
+            let url2 = URL(fileURLWithPath: videoFilePath!)
             
-            player.loadVideos(with: [url1, url2, url3])
+            player.loadVideos(with: [url1, url2])
             player.isToShowPlaybackControls = true
             player.isMuted = true
             player.playVideo()
